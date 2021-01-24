9125453ce118616debbb416375c060a91d6dc6af
Shreyas_hc1999 <55309631+shreyasmkbhd@users.noreply.github.com>
2021-01-24 11:32:57 +0530

    Add files via upload

---
+++ 7.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <meta http-equiv="refresh" content="1" />
+    <style>
+        p {
+            color: white;
+            font-size: 90px;
+            text-align:center;
+            position: absolute;
+            top: 50%;
+            left:30%;
+        }
+
+        body {
+            background-color: black;
+        }
+
+    </style>
+    <p> <?php echo date(" h: i : s A");?> </p>
+</head>
+
+</html>
